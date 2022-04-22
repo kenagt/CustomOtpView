ddfb05adc8c7bb9dbb53eefc5300918b4b1370a4
Kenan <begic_kenan@hotmail.com>
2022-04-22 20:45:45 +0200

    Initial Commit

---
+++ CustomOtpView/CustomOtpViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomOtpViewApp.swift
+//  CustomOtpView
+//
+//  Created by Kenan Begić on 22. 4. 2022..
+//
+
+import SwiftUI
+
+@main
+struct CustomOtpViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
